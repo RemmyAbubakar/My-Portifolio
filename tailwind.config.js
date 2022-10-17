eb44a9ac0de910a52a266ac9bf5109cdd6c21150
Remmy Abubakar <remmybello123@gmail.com>
2022-10-17 14:21:35 +0000

    portifolio

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: "class",
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
+    fontFamily: {
+      abc: ["Roboto Flex", "sans-serif"],
+      second: ["Urbanist", "sans-serif"],
+      remmy: ["Pangolin", "cursive"],
+    },
   },
   plugins: [],
 };
